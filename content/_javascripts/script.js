752591e4c11ccb9ff709b22ad713e1affbc25143
kizu <kizmarh@gmail.com>
2011-09-12 22:59:40 +0400

    Accordeon fixes

--- content/_javascripts/script.js
+++ content/_javascripts/script.js
@@ -2,9 +2,17 @@
 document.documentElement.id = "js"; 
 
 (function(){
+	var isOldIE = ($.browser.msie && parseInt($.browser.version,10) < 8),
+		isAniPhone = navigator.userAgent.indexOf("iPhone") != -1,
+		isAniPod = navigator.userAgent.indexOf("iPod") != -1,
+		isAniPad = navigator.userAgent.indexOf("iPad") != -1,
+		isAnIOS = isAniPhone || isAniPad || isAniPod;
+
 	// Boyans
 	$('.b-boyan-item').hover(function() {
 		if (!$(this).hasClass('b-boyan-item_current')) {
+			$(this).addClass('b-boyan-item_current').siblings('.b-boyan-item_current').removeClass('b-boyan-item_current');
+
 			var step = parseInt(500/($(this).parent().children().length-1),10)/10;
 			if ($(this).index()!=$(this).parent().children().length-1) {
 				$(this).closest('.b-boyan').removeClass('b-boyan_noshadow');
@@ -22,7 +30,6 @@ document.documentElement.id = "js";
 			$(this).animate({
 				left: $(this).index()*step + '%'
 			}, { queue: false, duration: 300, complete: function() {
-				$(this).addClass('b-boyan-item_current').siblings('.b-boyan-item_current').removeClass('b-boyan-item_current');
 				if ($(this).index()==$(this).parent().children().length-1) {
 					$(this).closest('.b-boyan').addClass('b-boyan_noshadow');
 				}
@@ -30,6 +37,16 @@ document.documentElement.id = "js";
 			return false;
 		}
 	});
+	
+	// Fixes for iOS
+	if (isAnIOS) {
+		// Pervent tap for noncurrent elements
+		$('.b-boyan-item').click(function() {
+			if (!$(this).hasClass('b-boyan-item_current')) {
+				return false;
+			}
+		});
+	}
 
 	// Init Boyans (maybe add random?)
 	$('.b-boyan').each(function() {
