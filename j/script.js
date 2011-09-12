2b021ef781abd4eff59cf789f7315526edd627d3
kizu <kizmarh@gmail.com>
2011-09-12 22:17:30 +0400

    Changed click to hover for accordion

--- j/script.js
+++ j/script.js
@@ -3,7 +3,7 @@ document.documentElement.id = "js";
 
 (function(){
 	// Boyans
-	$('.b-boyan-item').click(function() {
+	$('.b-boyan-item').hover(function() {
 		if (!$(this).hasClass('b-boyan-item_current')) {
 			var step = parseInt(500/($(this).parent().children().length-1),10)/10;
 			if ($(this).index()!=$(this).parent().children().length-1) {
@@ -12,21 +12,21 @@ document.documentElement.id = "js";
 			$(this).prevAll().each(function() {
 				$(this).animate({
 					left: $(this).index()*step + '%'
-				}, 300)
+				}, { queue: false, duration: 300 })
 			});
 			$(this).nextAll().each(function() {
 				$(this).animate({
 					left: 50+($(this).index()-1)*step + '%'
-				}, 300)
+				}, { queue: false, duration: 300 })
 			});
 			$(this).animate({
 				left: $(this).index()*step + '%'
-			}, 300, function() {
+			}, { queue: false, duration: 300, complete: function() {
 				$(this).addClass('b-boyan-item_current').siblings('.b-boyan-item_current').removeClass('b-boyan-item_current');
 				if ($(this).index()==$(this).parent().children().length-1) {
 					$(this).closest('.b-boyan').addClass('b-boyan_noshadow');
 				}
-			});
+			}});
 			return false;
 		}
 	});
