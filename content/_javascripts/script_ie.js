7a8cc2c2c29e28003b59a8cbe52cc11de98a0b65
kizu <kizmarh@gmail.com>
2011-10-01 18:41:09 +0400

    HTML5 doctor for IE

---
+++ content/_javascripts/script_ie.js
@@ -0,0 +1,2 @@
+// HTML5 doctor
+(function(){var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
