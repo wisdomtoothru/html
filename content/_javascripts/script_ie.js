93d0796ad1f732b39f18d63e9ea02ab1f033f2b9
kizu <kizmarh@gmail.com>
2011-10-23 19:32:58 +0400

    Some more fixes to IE

--- content/_javascripts/script_ie.js
+++ content/_javascripts/script_ie.js
@@ -1,5 +1,5 @@
 // HTML5 doctor
-(function(){var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
+(function(){var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,figcaption,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
 
 var IExpr = {
 
